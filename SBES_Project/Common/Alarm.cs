83b8328531f111da7322485772d2c5b324de309e
Srdjan Stankov <srki.miki@gmail.com>
2019-12-04 20:26:09 +0100

    Added Alarm class

--- SBES_Project/Common/Alarm.cs
+++ SBES_Project/Common/Alarm.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Common
 {
     public class Alarm
     {
+        public TimeSpan TimeOfAlarm { get; set; }
+
+        public string NamoOfClient { get; set; }
+
+        public string Message { get; set; }
+
+        public int Risk { get; set; }
+
+        public Alarm(TimeSpan timeOfAlarm, string namoOfClient, string message)
+        {
+            TimeOfAlarm = timeOfAlarm;
+            NamoOfClient = namoOfClient;
+            Message = message;
+            Risk = CalculateRisk();
+        }
+
+        private int CalculateRisk()
+        {
+            return new Random().Next(1, 11);
+        }
     }
 }
