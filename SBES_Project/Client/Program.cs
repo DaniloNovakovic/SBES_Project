f3376cab20f59c560a117e5d6d25de465ee205ec
Srdjan Stankov <srki.miki@gmail.com>
2019-12-05 15:09:19 +0100

    Created simple tcp client

--- SBES_Project/Client/Program.cs
+++ SBES_Project/Client/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Client
+﻿using System.ServiceModel;
+using System;
+using Common;
+
+namespace Client
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            using (var proxy = new ClientProxy(new NetTcpBinding(), new EndpointAddress("net.tcp://localhost:15000/PrimaryService")))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    proxy.SendAlarm(new Alarm(new TimeSpan(5 + i, 32 + i, 14 + i), $"Client {i}", $"Message {i}")); ;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
