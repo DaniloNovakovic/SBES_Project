546124ce9f8c80b31c72d33915514a841f87b54c
Srdjan Stankov <srki.miki@gmail.com>
2019-12-05 15:10:23 +0100

    Updated alarm class to work over network

--- SBES_Project/Common/Alarm.cs
+++ SBES_Project/Common/Alarm.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Common
 {
+    [DataContract]
     public class Alarm
     {
+        [DataMember]
         public TimeSpan TimeOfAlarm { get; set; }
 
+        [DataMember]
         public string NamoOfClient { get; set; }
 
+        [DataMember]
         public string Message { get; set; }
 
+        [DataMember]
         public int Risk { get; set; }
 
         public Alarm(TimeSpan timeOfAlarm, string namoOfClient, string message)
