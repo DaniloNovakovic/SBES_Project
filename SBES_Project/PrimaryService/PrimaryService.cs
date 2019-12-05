b1c82e944ba750cbf5c0c9d646a9d154166ca6c0
Srdjan Stankov <srki.miki@gmail.com>
2019-12-05 14:10:20 +0100

    Added test implementation of IPrimaryService

---
+++ SBES_Project/PrimaryService/PrimaryService.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using System;
+
+namespace PrimaryService
+{
+    public class PrimaryService : IPrimaryService
+    {
+        public void SendAlarm(Alarm alarm)
+        {
+            // TODO: smestanje u fajl/bazu uz proveru ovlascenja klijenta
+            // TODO: smestanje u buffer za repliciranje
+
+            Console.WriteLine($"Sending alarm: {alarm.Message}");
+        }
+    }
+}
